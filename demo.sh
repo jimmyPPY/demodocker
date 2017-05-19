3b85da6a989c7a0dfecd6a0a7bd32208fb2f332c
root <root@POCDOCKER2.echosens.dev>
2017-05-19 10:58:01 +0200

    change index.html

--- demo.sh
+++ demo.sh
@@ -7,10 +7,12 @@
 # 
 ######################################
 pause='read -p "################################################################"'
+BLUE='\033[0;34m'
+NC='\033[0m'
 clear
 set -x
 # Installation
-echo 'curl -fsSL https://get.docker.com/ | sh'
+echo -e "${BLUE}curl -fsSL https://get.docker.com/ | sh ${NC}"
 #systemctl start docker
 #systemctl status docker
 #systemctl enable docker
@@ -24,53 +26,55 @@ $pause
 docker pull hello-world
 docker images
 $pause
-docker ps
-$pause
 docker run hello-world
-docker ps
+docker ps -a
+$pause
+clear
+echo -e "${BLUE}ISOLATION DEMO ${NC}"
+echo -e "${BLUE}filesystem, processus, network ${NC}"
 docker run -it busybox
-echo 'ISOLATION DEMO'
-ls
+ls /
 $pause
 ps
 $pause
 ifconfig
 $pause
-exit
 docker images
-docker ps
 $pause
-echo 'docker rm'
-echo 'docker rmi hello-world'
+echo -e "${BLUE}docker rm ${NC}"
+echo -e "${BLUE}docker rmi hello-world ${NC}"
+$pause
+clear
 docker inspect busybox
 $pause
+clear
+echo -e "${BLUE}Personalisation de conteneur ${NC}"
 docker run --name welcomedock busybox echo WELCOME IN DOCKER WORLD
 $pause
+docker ps -a
 docker start -a welcomedock
 $pause
 docker commit welcomedock mywelcomeimg
 $pause
 docker images
 $pause
-docker inspect mywelcomeimg
-docker inspect welcomedock
 docker history mywelcomeimg
 $pause
 clear
-echo 'Docker Registry'
+echo -e "${BLUE}Docker Registry ${NC}"
 docker search --filter "is-official=true" a
 $pause
-docker tag mywelcomeimg jimmyppy/mywelcomeapp
+docker tag mywelcomeimg jimmyppy/mywelcomeapp:v1
 #docker login
 docker push jimmyppy/mywelcomeapp:v1
 $pause
 clear
-echo 'Docker run option'
+echo -e "${BLUE}Docker run option ${NC}"
 docker run -it -p 80:80 httpd /bin/bash
 $pause
 docker run -dit -p 80:80 httpd apachectl -D FOREGROUND
 $pause
-echo 'Run with volume mapping'
+echo -e "${BLUE}Run with volume mapping ${NC}"
 cd $HOME/demodocker/httpd/
 ls
 $pause
@@ -81,7 +85,7 @@ $pause
 docker stop volmapping
 cd $HOME/demodocker/httpd/
 clear
-echo 'Le dockerfile'
+echo -e "${BLUE}Le dockerfile ${NC}"
 cat dockerfile.yml
 $pause
 docker build -t myhttpd .
@@ -93,15 +97,16 @@ $pause
 
 clear
 cd $HOME/demodocker/webapp/
-echo 'Docker link option'
+echo -e "${BLUE}Docker link option ${NC}"
 docker run  --name esmysql -e MYSQL_ROOT_PASSWORD=passe -d mysql/mysql-server:5.7
 $pause
 docker exec -it esmysql mysql -uroot -p -e "GRANT ALL PRIVILEGES ON *.* TO 'root@%';FLUSH PRIVILEGES;"
 $pause
+echo -e "${BLUE}Check 
+ ${NC}"
 docker exec -it esmysql mysql -uroot -p
 connect
 SHOW DATABASES;
-exit
 $pause
 docker exec -it esmysql mysql -uroot -p -e "GRANT ALL PRIVILEGES ON *.* TO 'root@%';FLUSH PRIVILEGES;"
 docker run -dit --name myrunwebapp -p 80:80 --link esmysql:msql -v "$PWD:/var/www/html" mywebapp apachectl -D FOREGROUND
@@ -109,7 +114,7 @@ docker exec -it myrunwebapp /bin/bash -c env
 $pause
 
 clear
-echo 'Docker Compose'
+echo -e "${BLUE}Docker Compose ${NC}"
 cat docker-compose.yml
 $pause
 docker-compose up
@@ -119,7 +124,7 @@ $pause
 docker-compose down
 $pause
 clear
-echo 'Docker Compose => Scalabilité'
+echo -e "${BLUE}Docker Compose => Scalabilité ${NC}"
 cd $HOME/demodocker/HAdemo/
 cat docker-compose.yml
 $pause
@@ -135,7 +140,7 @@ docker-compose logs
 $pause
 
 clear
-echo 'Docker SWARM'
+echo -e "${BLUE}Docker SWARM ${NC}"
 docker node ls
 $pause
 docker service create --replicas 1 --name helloworld busybox ping docker.com
@@ -153,7 +158,7 @@ docker service rm helloworld
 
 
 clear
-echo 'Docker Tools'
+echo -e "${BLUE}Docker Tools ${NC}"
 docker run -v "/var/run/docker.sock:/var/run/docker.sock" -p 9000:9000 portainer/portainer
 $pause
 docker run -d --restart=unless-stopped -p 8080:8080 rancher/server
